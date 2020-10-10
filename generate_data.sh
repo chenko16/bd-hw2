79f046bf86d306032be00102001acbfaeb9229d4
chenko16 <Durachenko-serg@yandex.ru>
2020-10-10 13:01:07 -0700

    Added metric generation script

---
+++ generate_data.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cur_date=$(date +"%s") # get current datetime
+rows=1000
+groups=2
+
+echo "Start generation..."
+
+# generate csv file with structure [id, timestamp, value]
+i=1
+while [ "$i" -le "$rows" ]
+do
+  tmp_date=$((cur_date + i * 10)) # increment time by 10 seconds each iteration
+
+  id=1
+  while [ "$id" -le "$groups" ] # generate metric for each group
+  do
+    value=$((RANDOM % 10 * 10)) # generate random value
+    echo "$id, $tmp_date, $value" >> log.csv
+    id=$((id + 1))
+  done
+
+  i=$((i + 1))
+done
+
+echo "Finish generation"
